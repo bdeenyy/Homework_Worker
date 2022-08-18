1eebf3710dc5e71bdfe358f3ea1f4a73a471f44e
bdeenyy <109365710+bdeenyy@users.noreply.github.com>
2022-08-18 18:14:16 +0300

    update final

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Worker.OnTaskDoneListener listener = System.out::println;
-        Worker worker = new Worker(listener);
+        Worker.OnTaskDoneListener listenerOne = System.out::println;
+        Worker.OnTaskErrorListener listenerTwo = System.out::println;
+        Worker worker = new Worker(listenerOne, listenerTwo);
         worker.start();
     }
 }
--- src/Worker.java
+++ src/Worker.java
@@ -10,12 +10,17 @@ public class Worker {
     public interface OnTaskErrorListener {
         void onError(String result);
     }
-    public Worker(OnTaskDoneListener callback) {
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
         this.callback = callback;
+        this.errorCallback = errorCallback;
     }
     public void start() {
         for (int i = 0; i < 100; i++) {
-            callback.onDone("Task " + i + " is done");
+            if (i != 33){
+                callback.onDone("Task " + i + " is done");
+            } else {
+                errorCallback.onError("Task " + i + " is error");
+            }
         }
     }
 }
